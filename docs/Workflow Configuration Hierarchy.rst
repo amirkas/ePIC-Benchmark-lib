677d6c193253206f5fe26799b568eb71d65e3f61
amirkas <amirkabdou@gmail.com>
2025-05-25 15:45:17 -0700

    starting documentation

---
+++ docs/Workflow Configuration Hierarchy.rst
@@ -0,0 +1,3 @@
+Workflow Configuration Hierarchy
+=====================
+
--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "ePIC Workflows"
+copyright = "2025, Lawrence Berkeley National Laboratory"
+author = "Amir Abdou"
 
 
 # -- General configuration ---------------------------------------------------
