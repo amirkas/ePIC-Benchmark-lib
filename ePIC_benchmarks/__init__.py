95133aa5c0087b578600075553988c5176e5e48f
amirkas <amirkabdou@gmail.com>
2025-05-26 19:05:33 -0700

    moved package into src

--- ePIC_benchmarks/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import (analysis, benchmark, container, detector, parsl, simulation, workflow)
-
-__all__ = ['analysis', 'benchmark', 'container', 'detector', 'parsl', 'simulation', 'workflow']
-
