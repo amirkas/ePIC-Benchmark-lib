c985882a3009606b0ba50b4577831c775f99b314
amirkas <amirkabdou@gmail.com>
2025-05-26 19:19:35 -0700

    testing different conf.py path

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('./src/ePIC_benchmarks'))
+sys.path.insert(0, os.path.abspath('./src'))
+
 
 
 # -- Project information -----------------------------------------------------
